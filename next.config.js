a15464e0748a89498d264c53a8fbe7d86a4a377f
Trevor Henderson <trevorsgit@gmail.com>
2020-11-25 01:25:35 -0600

    Stop for the night

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    distDir: 'build',
+    publicRuntimeConfig: {
+        //NEXT_PUBLIC_
+    },
+    webpack: (config) => {
+        return config;
+    }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react';
+
+const IndexPage: FC = () => {
+    return <h1>What's, Next?</h1>
+};
+
+export default IndexPage;
